68b32db6d07e8a96ba87f75402cb3c32e244402c
Antonio Colapietro <36625268+tonyco97@users.noreply.github.com>
2021-03-18 14:25:52 +0100

    Add files via upload

---
+++ function/request.js
@@ -0,0 +1,569 @@
+const apiResp = [
+  {
+    installations: "7.7.1908#1,7.8.2003#1,6.10#2,7.5.1804#1,7.9.2009#30",
+    country_code: "",
+    country_name: "",
+  },
+  {
+    installations: "7.9.2009#6",
+    country_code: "AL",
+    country_name: "Albania",
+  },
+  {
+    installations: "7.6.1810#2,7.8.2003#1,6.10#1",
+    country_code: "AO",
+    country_name: "Angola",
+  },
+  {
+    installations:
+      "7.9.2009#9,7.6.1810#16,7.7.1908#19,7.8.2003#25,6.10#8,7.5.1804#3",
+    country_code: "AR",
+    country_name: "Argentina",
+  },
+  {
+    installations: "7.8.2003#6,7.9.2009#39",
+    country_code: "AT",
+    country_name: "Austria",
+  },
+  {
+    installations: "6.10#1,7.8.2003#10,7.6.1810#1,7.7.1908#4,7.9.2009#55",
+    country_code: "AU",
+    country_name: "Australia",
+  },
+  {
+    installations: "7.9.2009#2",
+    country_code: "BA",
+    country_name: "Bosnia and Herzegovina",
+  },
+  {
+    installations: "7.6.1810#5,7.7.1908#2,7.9.2009#20,7.8.2003#4",
+    country_code: "BE",
+    country_name: "Belgium",
+  },
+  {
+    installations: "7.5.1804#1,7.6.1810#1,7.7.1908#1,7.9.2009#6",
+    country_code: "BG",
+    country_name: "Bulgaria",
+  },
+  {
+    installations: "7.9.2009#2",
+    country_code: "BM",
+    country_name: "Bermuda",
+  },
+  {
+    installations: "7.9.2009#1,7.5.1804#1,7.6.1810#1,7.8.2003#1",
+    country_code: "BO",
+    country_name: "Bolivia",
+  },
+  {
+    installations:
+      "7.8.2003#62,6.10#14,6.9#1,7.7.1908#36,7.9.2009#159,7.5.1804#9,7.4.1708#1,7.6.1810#35",
+    country_code: "BR",
+    country_name: "Brazil",
+  },
+  {
+    installations: "7.7.1908#1",
+    country_code: "BW",
+    country_name: "Botswana",
+  },
+  {
+    installations: "7.6.1810#9,7.7.1908#1,7.9.2009#5",
+    country_code: "BY",
+    country_name: "Belarus",
+  },
+  {
+    installations: "7.9.2009#2",
+    country_code: "BZ",
+    country_name: "Belize",
+  },
+  {
+    installations: "7.6.1810#2,7.7.1908#14,7.9.2009#75,7.8.2003#19",
+    country_code: "CA",
+    country_name: "Canada",
+  },
+  {
+    installations: "6.10#4,7.6.1810#3,7.7.1908#1,7.9.2009#48,7.8.2003#4",
+    country_code: "CH",
+    country_name: "Switzerland",
+  },
+  {
+    installations: "7.7.1908#1,7.9.2009#8,7.8.2003#2",
+    country_code: "CL",
+    country_name: "Chile",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "CM",
+    country_name: "Cameroon",
+  },
+  {
+    installations: "7.4.1708#1,7.7.1908#1,7.8.2003#5,7.9.2009#10,6.10#2",
+    country_code: "CN",
+    country_name: "China",
+  },
+  {
+    installations:
+      "7.4.1708#1,7.3.1611#1,7.7.1908#5,7.8.2003#9,7.9.2009#20,7.6.1810#4",
+    country_code: "CO",
+    country_name: "Colombia",
+  },
+  {
+    installations: "7.9.2009#1,7.7.1908#2",
+    country_code: "CR",
+    country_name: "Costa Rica",
+  },
+  {
+    installations: "7.5.1804#3,7.6.1810#1,7.9.2009#4,7.8.2003#4",
+    country_code: "CY",
+    country_name: "Cyprus",
+  },
+  {
+    installations: "7.7.1908#2,7.8.2003#7,6.10#3,7.6.1810#1,7.9.2009#12",
+    country_code: "CZ",
+    country_name: "Czech Republic",
+  },
+  {
+    installations:
+      "7.5.1804#4,7.7.1908#25,7.8.2003#56,6.10#13,7.4.1708#1,6.9#1,7.6.1810#15,7.9.2009#385",
+    country_code: "DE",
+    country_name: "Germany",
+  },
+  {
+    installations: "7.9.2009#24,7.7.1908#2,6.10#1",
+    country_code: "DK",
+    country_name: "Denmark",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "DO",
+    country_name: "Dominican Republic",
+  },
+  {
+    installations: "7.8.2003#1",
+    country_code: "DZ",
+    country_name: "Algeria",
+  },
+  {
+    installations:
+      "7.8.2003#1,6.10#1,7.6.1810#3,7.2.1511#1,7.5.1804#1,7.9.2009#7,7.7.1908#5",
+    country_code: "EC",
+    country_name: "Ecuador",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "EE",
+    country_name: "Estonia",
+  },
+  {
+    installations: "7.7.1908#2,7.8.2003#3,7.9.2009#2",
+    country_code: "EG",
+    country_name: "Egypt",
+  },
+  {
+    installations:
+      "6.10#4,7.6.1810#5,7.7.1908#3,7.8.2003#9,7.9.2009#38,7.5.1804#7",
+    country_code: "ES",
+    country_name: "Spain",
+  },
+  {
+    installations: "7.7.1908#1,7.9.2009#2",
+    country_code: "EU",
+    country_name: "Europe",
+  },
+  {
+    installations: "7.7.1908#2,7.9.2009#12,7.6.1810#3,7.8.2003#2",
+    country_code: "FI",
+    country_name: "Finland",
+  },
+  {
+    installations:
+      "7.5.1804#3,7.8.2003#26,7.7.1908#10,7.9.2009#170,6.10#5,7.6.1810#7",
+    country_code: "FR",
+    country_name: "France",
+  },
+  {
+    installations:
+      "7.6.1810#7,7.5.1804#2,7.8.2003#14,7.7.1908#10,7.9.2009#80,6.10#12",
+    country_code: "GB",
+    country_name: "United Kingdom",
+  },
+  {
+    installations: "7.9.2009#1,7.8.2003#6",
+    country_code: "GF",
+    country_name: "French Guiana",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "GG",
+    country_name: "Guernsey",
+  },
+  {
+    installations: "7.6.1810#1",
+    country_code: "GI",
+    country_name: "Gibraltar",
+  },
+  {
+    installations: "7.9.2009#5",
+    country_code: "GP",
+    country_name: "Guadeloupe",
+  },
+  {
+    installations: "7.5.1804#2,7.6.1810#1,7.9.2009#8,7.7.1908#2,7.8.2003#1",
+    country_code: "GR",
+    country_name: "Greece",
+  },
+  {
+    installations: "7.6.1810#2,7.9.2009#7,7.7.1908#1,7.8.2003#3,7.5.1804#1",
+    country_code: "GT",
+    country_name: "Guatemala",
+  },
+  {
+    installations: "7.6.1810#2,7.9.2009#8,7.8.2003#1",
+    country_code: "HK",
+    country_name: "Hong Kong",
+  },
+  {
+    installations: "7.5.1804#2,7.7.1908#3,7.6.1810#3,7.9.2009#9,7.8.2003#3",
+    country_code: "HR",
+    country_name: "Croatia",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "HT",
+    country_name: "Haiti",
+  },
+  {
+    installations:
+      "7.8.2003#19,6.10#3,7.9.2009#24,7.5.1804#2,7.7.1908#6,7.6.1810#6",
+    country_code: "HU",
+    country_name: "Hungary",
+  },
+  {
+    installations: "7.7.1908#1,7.6.1810#1,7.8.2003#4,7.9.2009#5,7.5.1804#1",
+    country_code: "ID",
+    country_name: "Indonesia",
+  },
+  {
+    installations: "7.6.1810#1,7.8.2003#1,7.9.2009#7",
+    country_code: "IE",
+    country_name: "Ireland",
+  },
+  {
+    installations: "7.6.1810#1,7.8.2003#2",
+    country_code: "IL",
+    country_name: "Israel",
+  },
+  {
+    installations: "7.6.1810#1,7.9.2009#12,7.8.2003#3,7.7.1908#1",
+    country_code: "IN",
+    country_name: "India",
+  },
+  {
+    installations: "7.6.1810#1,7.9.2009#31,6.10#2,7.8.2003#1",
+    country_code: "IR",
+    country_name: "Iran, Islamic Republic of",
+  },
+  { installations: "6.10#1", country_code: "IS", country_name: "Iceland" },
+  {
+    installations:
+      "6.10#2163,6.9#7,7.5.1804#64,6.8#2,7.6.1810#296,7.7.1908#292,7.9.2009#11819,7.8.2003#1122,7.4.1708#16,6.7#1",
+    country_code: "IT",
+    country_name: "Italy",
+  },
+  {
+    installations: "7.6.1810#1",
+    country_code: "JM",
+    country_name: "Jamaica",
+  },
+  {
+    installations: "7.9.2009#6,7.8.2003#1,7.6.1810#2,7.4.1708#1",
+    country_code: "JP",
+    country_name: "Japan",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "KE",
+    country_name: "Kenya",
+  },
+  {
+    installations: "7.8.2003#1,7.6.1810#2,7.7.1908#1",
+    country_code: "KG",
+    country_name: "Kyrgyzstan",
+  },
+  {
+    installations: "7.9.2009#2",
+    country_code: "KR",
+    country_name: "Korea, Republic of",
+  },
+  {
+    installations: "7.7.1908#1,7.8.2003#3,7.9.2009#3",
+    country_code: "KZ",
+    country_name: "Kazakhstan",
+  },
+  {
+    installations: "7.5.1804#1",
+    country_code: "LK",
+    country_name: "Sri Lanka",
+  },
+  {
+    installations: "7.9.2009#19,7.5.1804#1,7.8.2003#3",
+    country_code: "LT",
+    country_name: "Lithuania",
+  },
+  {
+    installations: "7.8.2003#2,7.9.2009#1",
+    country_code: "LV",
+    country_name: "Latvia",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "LY",
+    country_name: "Libya",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "MA",
+    country_name: "Morocco",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "MC",
+    country_name: "Monaco",
+  },
+  {
+    installations: "7.8.2003#1,7.9.2009#1",
+    country_code: "MD",
+    country_name: "Moldova, Republic of",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "MK",
+    country_name: "Macedonia",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "MQ",
+    country_name: "Martinique",
+  },
+  {
+    installations: "7.9.2009#2,7.6.1810#2,7.8.2003#3,7.5.1804#1",
+    country_code: "MU",
+    country_name: "Mauritius",
+  },
+  {
+    installations: "7.8.2003#7,7.5.1804#1,6.10#1,7.7.1908#5,7.9.2009#36",
+    country_code: "MX",
+    country_name: "Mexico",
+  },
+  {
+    installations:
+      "7.6.1810#1,7.8.2003#4,6.9#1,6.10#5,7.7.1908#1,7.9.2009#1",
+    country_code: "MY",
+    country_name: "Malaysia",
+  },
+  {
+    installations: "7.9.2009#1,7.6.1810#1",
+    country_code: "MZ",
+    country_name: "Mozambique",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "NA",
+    country_name: "Namibia",
+  },
+  {
+    installations: "7.9.2009#2",
+    country_code: "NE",
+    country_name: "Niger",
+  },
+  {
+    installations: "7.8.2003#1",
+    country_code: "NI",
+    country_name: "Nicaragua",
+  },
+  {
+    installations:
+      "7.7.1908#11,7.9.2009#129,7.5.1804#2,7.6.1810#4,6.10#2,7.8.2003#11",
+    country_code: "NL",
+    country_name: "Netherlands",
+  },
+  {
+    installations: "7.9.2009#6,7.6.1810#1",
+    country_code: "NO",
+    country_name: "Norway",
+  },
+  {
+    installations: "7.6.1810#1,7.8.2003#2,7.7.1908#1,7.9.2009#9",
+    country_code: "NZ",
+    country_name: "New Zealand",
+  },
+  {
+    installations: "7.9.2009#3",
+    country_code: "PA",
+    country_name: "Panama",
+  },
+  {
+    installations: "7.9.2009#4,7.8.2003#2,7.7.1908#2,7.5.1804#1",
+    country_code: "PE",
+    country_name: "Peru",
+  },
+  {
+    installations:
+      "7.5.1804#8,7.6.1810#6,7.4.1708#2,7.9.2009#6,7.8.2003#8,7.7.1908#7",
+    country_code: "PH",
+    country_name: "Philippines",
+  },
+  {
+    installations: "7.8.2003#1,7.6.1810#2,7.9.2009#2",
+    country_code: "PK",
+    country_name: "Pakistan",
+  },
+  {
+    installations:
+      "7.9.2009#40,6.10#1,7.7.1908#12,7.8.2003#15,7.5.1804#2,7.4.1708#1,7.6.1810#8",
+    country_code: "PL",
+    country_name: "Poland",
+  },
+  {
+    installations: "7.8.2003#4",
+    country_code: "PS",
+    country_name: "Palestinian Territory",
+  },
+  {
+    installations: "7.6.1810#10,7.9.2009#22,6.10#1,7.7.1908#1,7.8.2003#3",
+    country_code: "PT",
+    country_name: "Portugal",
+  },
+  {
+    installations: "7.8.2003#6,7.6.1810#4,7.9.2009#2,6.10#1,7.7.1908#3",
+    country_code: "PY",
+    country_name: "Paraguay",
+  },
+  {
+    installations:
+      "6.10#1,7.7.1908#1,7.8.2003#6,7.5.1804#1,7.6.1810#3,7.9.2009#22",
+    country_code: "RO",
+    country_name: "Romania",
+  },
+  {
+    installations: "7.9.2009#1,7.7.1908#1,7.8.2003#4",
+    country_code: "RS",
+    country_name: "Serbia",
+  },
+  {
+    installations:
+      "7.4.1708#1,7.6.1810#6,6.10#3,7.9.2009#99,7.7.1908#14,6.9#1,7.5.1804#2,7.8.2003#34",
+    country_code: "RU",
+    country_name: "Russian Federation",
+  },
+  {
+    installations: "6.10#1",
+    country_code: "SA",
+    country_name: "Saudi Arabia",
+  },
+  {
+    installations: "7.8.2003#6,7.6.1810#2,7.9.2009#20,7.7.1908#7",
+    country_code: "SE",
+    country_name: "Sweden",
+  },
+  {
+    installations: "7.9.2009#2,7.8.2003#4",
+    country_code: "SG",
+    country_name: "Singapore",
+  },
+  {
+    installations: "7.5.1804#1,7.9.2009#3",
+    country_code: "SI",
+    country_name: "Slovenia",
+  },
+  {
+    installations: "7.7.1908#2,7.9.2009#9,7.6.1810#3,7.8.2003#9",
+    country_code: "SK",
+    country_name: "Slovakia",
+  },
+  {
+    installations: "6.10#3,7.9.2009#16",
+    country_code: "SM",
+    country_name: "San Marino",
+  },
+  {
+    installations: "7.6.1810#1",
+    country_code: "SR",
+    country_name: "Suriname",
+  },
+  {
+    installations: "7.7.1908#2",
+    country_code: "SV",
+    country_name: "El Salvador",
+  },
+  {
+    installations: "7.9.2009#1,7.5.1804#1",
+    country_code: "SZ",
+    country_name: "Swaziland",
+  },
+  {
+    installations: "7.6.1810#1,7.9.2009#8,7.7.1908#1",
+    country_code: "TH",
+    country_name: "Thailand",
+  },
+  {
+    installations: "7.6.1810#1,7.9.2009#1,7.7.1908#1",
+    country_code: "TN",
+    country_name: "Tunisia",
+  },
+  {
+    installations:
+      "7.8.2003#3,7.7.1908#3,7.5.1804#3,7.6.1810#1,7.9.2009#10",
+    country_code: "TR",
+    country_name: "Turkey",
+  },
+  {
+    installations: "7.9.2009#3",
+    country_code: "TW",
+    country_name: "Taiwan",
+  },
+  {
+    installations:
+      "7.5.1804#1,7.6.1810#3,6.10#1,7.8.2003#5,7.7.1908#8,7.9.2009#44",
+    country_code: "UA",
+    country_name: "Ukraine",
+  },
+  {
+    installations:
+      "7.4.1708#4,7.5.1804#10,7.6.1810#21,6.10#7,7.8.2003#61,7.7.1908#33,7.9.2009#288",
+    country_code: "US",
+    country_name: "United States",
+  },
+  {
+    installations: "7.7.1908#1,7.9.2009#5,6.9#1",
+    country_code: "UY",
+    country_name: "Uruguay",
+  },
+  {
+    installations: "7.7.1908#2,7.9.2009#5,7.8.2003#2",
+    country_code: "UZ",
+    country_name: "Uzbekistan",
+  },
+  {
+    installations: "7.9.2009#1",
+    country_code: "VA",
+    country_name: "Holy See (Vatican City State)",
+  },
+  {
+    installations: "7.5.1804#1,7.7.1908#3,7.9.2009#4",
+    country_code: "VE",
+    country_name: "Venezuela",
+  },
+  {
+    installations: "7.8.2003#1,7.9.2009#4,7.6.1810#1",
+    country_code: "VN",
+    country_name: "Vietnam",
+  },
+  {
+    installations:
+      "7.8.2003#5,7.9.2009#23,6.10#1,7.5.1804#1,7.6.1810#4,7.7.1908#3",
+    country_code: "ZA",
+    country_name: "South Africa",
+  },
+];
+
